a3cbe050c89f974f2659e513ce68d40673b6077c
SergioVed <vedankinsergij@gmail.com>
2024-08-07 18:29:06 +0300

    HomePage + Header fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import './App.css';
-import { Header } from './components/header';
 import { HomePage } from './components/homePage';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
       <HomePage />
     </div>
   );
--- client/src/components/homePage/index.tsx
+++ client/src/components/homePage/index.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { Banners } from "./banners";
 import './style.scss'
+import { Header } from "../header";
 
 
 export const HomePage = () => {
     return (
         <div className="homepage">
+        <Header/>
             <div className="homepage__gradient"></div>
 
             <div className="homepage__container">
