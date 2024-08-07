b1fedb94678a02e3a67d24a3d1f0132e6cddf243
SergioVed <vedankinsergij@gmail.com>
2024-08-07 18:09:36 +0300

    header fix

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,8 +1,9 @@
 import "./index.scss"
 import { Element, elements } from "../../data/header"
 import { Input } from "../input"
-import profileImg from "../../img/header/profile.png"
 import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 
 export const Header = () => {
 
@@ -15,7 +16,7 @@ export const Header = () => {
             </div>
             <div className="header__inputContainer">
                 <Input/>
-                <img src={profileImg} alt="profileImg" className="header__inputContainer__img"/>
+                <Link to={""}><FontAwesomeIcon icon={faCircleUser} className="header__inputContainer__profileImg"/></Link>
             </div>
         </div>
     )
--- client/src/components/input/index.tsx
+++ client/src/components/input/index.tsx
@@ -1,11 +1,13 @@
 import "./index.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export const Input = () => {
 
     return(
         <form className="inputWrapper">
             <input type="text" className="inputWrapper__input"/>
-            <button className="inputWrapper__button" type="submit"></button>
+            <button className="inputWrapper__button" type="submit"><FontAwesomeIcon icon={faMagnifyingGlass}/></button>
         </form>
     )
 }
