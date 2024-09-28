c23ea8ef31bc3371bc57ffcd5612c7db652c5a4f
SergioVed <vedankinsergij@gmail.com>
2024-09-28 12:13:24 +0300

    merge + fix

--- client/src/components/randomWorkers/randomWorkersComponent.tsx
+++ client/src/components/randomWorkers/randomWorkersComponent.tsx
@@ -6,12 +6,8 @@ import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { UserCard } from "./workerCard";
 
-// import userImg from "../img/randomWorkers/user.png";
-
 export const TopWorkers = () => {
 
-    // console.log(best(workers))
-    // console.log(getNew(workers))
     const [title, setTitle] = useState("")
 
     const newWorkers: Worker[] = getNew(workers)
@@ -53,50 +49,3 @@ export const TopWorkers = () => {
         </div>
     )
 }
-
-// import "./index.scss"
-// import { workers, Worker } from "../../data/bestWorkersTest"
-// import userImg from "../img/randomWorkers/user.png";
-// import { useState } from "react";
-
-// export const TopWorkers = () => {
-
-//     const [currentIndex, setCurrentIndex] = useState(0);
-    
-//     const handleNext = () => {
-//         setCurrentIndex((prevIndex) => (prevIndex + 1) % arrays.length);
-//     };
-    
-//     const bestWorkers = [...workers].sort((a, b) => {
-//         return b.rating - a.rating
-//     })
-//     const worstWorkers = [...workers].sort((a, b) => {
-//         return a.rating - b.rating
-//     })
-//     const newestWorkers = [...workers].sort((a, b) => {
-//         return b.date.getTime() - a.date.getTime()
-//     })
-
-//     const arrays = [bestWorkers, worstWorkers, newestWorkers]
-
-    
-//     return(
-//         <div className="workers">
-//             <div className="workers__text">
-//                 <h1>Наші найкращі спеціалісти</h1>
-//                 <p>Вручну перевірені таланти для усіх ваших професійних потреб</p>
-//             </div>
-//             <div className="workers__container">
-//                 <button className="worker__container__nextBtn" onClick={handleNext}>Next</button>
-//                 {arrays[currentIndex].slice(0, 5).map((e: Worker) => (
-//                     <div className="workers__container__user">
-//                         <img src={e.img} alt="userImg" className="workers__container__user-img"/>
-//                         <p className="workers__container__user-name">{e.name}</p>
-//                         <p className="workers__container__user-work">{e.work}</p>
-//                     </div>
-//                 ))}
-//                 <button className="worker__container__previousBtn">Previous</button>
-//             </div>
-//         </div>
-//     )
-// }
